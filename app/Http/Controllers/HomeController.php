23a3e5b833edb0e3eee7a9ee716ff8e34fb34fe1
MrGians <gianlumura97@gmail.com>
2022-09-19 16:40:38 +0200

    add Route from controller

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -4,7 +4,14 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
+use App\Movie;
+
 class HomeController extends Controller
 {
-    //
+    public function index()
+    {
+        $movies = Movie::all();
+        dump($movies);
+        return view('movies.index', compact('movies'));
+    }
 }
