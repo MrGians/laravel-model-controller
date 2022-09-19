6f1d44c3ccaf7c6ea8fc804969273ba110005e9c
MrGians <gianlumura97@gmail.com>
2022-09-19 17:30:26 +0200

    Movies page Done

--- resources/views/includes/header.blade.php
+++ resources/views/includes/header.blade.php
@@ -1,13 +1,13 @@
 <nav class="navbar navbar-expand-lg bg-light">
   <div class="container-fluid">
-    <a class="navbar-brand" href="{{ route('movies') }}">Web Movies</a>
+    <a class="navbar-brand" href="{{ route('movies.index') }}">Web Movies</a>
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
     <div class="collapse navbar-collapse" id="navbarNav">
       <ul class="navbar-nav">
         <li class="nav-item">
-          <a class="nav-link active" aria-current="page" href="{{ route('movies') }}">Movies</a>
+          <a class="nav-link @if(Route::is('home'))active @endif" aria-current="page" href="{{ route('movies.index') }}">Movies</a>
         </li>
       </ul>
     </div>
--- resources/views/movies/index.blade.php
+++ resources/views/movies/index.blade.php
@@ -1,5 +1,32 @@
 @extends('layouts.main')
 
 @section('main')
-    
+{{-- Best Movies Section --}}
+  <section id="best-movies" class="my-4">
+    <div class="container">
+      <h2 class="text-center mb-3">Lista dei film più visti su Web Movies</h2>
+      {{-- Movies Tier List --}}
+      <ol class="list-group list-group-numbered">
+        @forelse($movies as $movie)
+        <li class="list-group-item d-flex justify-content-between align-items-start">
+          {{-- Title & Date --}}
+          <div class="ms-2 me-auto">
+            <div class="fw-bold">{{ $movie->title }}</div>
+            Data: {{ $movie->date }}
+          </div>
+          {{-- Single Movie Link --}}
+          <div class="ms-auto me-5">
+            <a class="show-movie" href="{{ route('movies.show', ['id' => $movie->id]) }}">Vai al film!</a>
+          </div>
+          {{-- Vote --}}
+          <span class="badge bg-primary rounded-pill">Voto: {{ $movie->vote }}</span>
+        </li>
+        @empty
+        <li>
+          <h4>Nessun film trovato.</h4>
+        </li>
+        @endforelse
+      </ol>
+    </div>
+  </section>   
 @endsection
